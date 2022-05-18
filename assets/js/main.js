f118745315cf9e471d54dba27f35d6dec50d1d96
EnzoCam7 <camerac09@gmail.com>
2022-05-18 19:11:17 -0300

    added scrollHeader function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
+function scrollHeader(){
+    const header = document.getElementById('header');
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+//When the scroll is greather than 50vw height, add the scroll-header calss to the header tag.
+window.addEventListener('scroll', scrollHeader)
 /*=============== SWIPER POPULAR ===============*/
 
 
