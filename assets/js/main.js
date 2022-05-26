a1679f14707bace5bb29d9b123a0f9ed3f913a65
EnzoCam7 <camerac09@gmail.com>
2022-05-26 10:05:50 -0300

    Value and Popular Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,48 @@ function scrollHeader(){
 //When the scroll is greather than 50vw height, add the scroll-header calss to the header tag.
 window.addEventListener('scroll', scrollHeader)
 /*=============== SWIPER POPULAR ===============*/
-
+var swiper = new Swiper(".popular_container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    navigation: {
+    nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value_accordion-item')
 
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value_accordion-header')
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(openItem && openItem !== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value_accordion-content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')    
+    }
+
+
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
